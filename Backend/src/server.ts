c3a8a9d484fc0ee6c697a8517852f44711a038c4
Nabin Agrawal <nabinagrawal64@gmail.com>
2026-05-12 14:24:47 +0530

    update mminor changes in backend

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -13,11 +13,12 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const allowedOrigins = process.env.FRONTEND_URL?.split(",") || [];
 app.use(cors({
-    origin: "http://localhost:5173", // Frontend URL
-    credentials: true, // Allow cookies to be sent
+    origin: allowedOrigins,
+    credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
